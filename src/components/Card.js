be2fc1ceea47d71ab15dbe1d1085cfe6bf5e6eba
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-05-28 21:40:19 -0400

    adding more info to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,35 @@ import '../assets/css/Card.css'
 export default function Card({character}) {
     return (
         <div className='card'>
-            <img 
-                src={character.image} 
-                width='220'
-                 style={{borderTopLeftRadius: '6px', borderBottomLeftRadius: '6px'}} 
+            <img
+                alt='profile' 
+                src={character.image}
+                className='img-profile'
             />
             <div className='info-container'>
-                {character.name}
+                <div className='info-item'>
+                    <strong>Nombre: </strong> {character.name}
+                </div>
+                <div className='info-item'>
+                    <strong>Especie: </strong> {character.species}
+                </div>
+                <div className='info-item'>
+                    <strong>Genero: </strong> {character.gender}
+                </div>
+                <div className='info-item'>
+                    <strong>Estatus: </strong> 
+                    <p
+                        style={{display: 'inline', color: character.status === 'Alive' ? 'green' : character.status === 'Dead' ? 'tomato' : 'orange'}}
+                    >
+                        {character.status}
+                    </p>
+                </div>
+                <div className='info-item'>
+                    <strong>Primera vez visto: </strong> {character.origin.name}
+                </div>
+                <div className='info-item'>
+                    <strong>Ultima vez visto: </strong> {character.location.name}
+                </div>
             </div>
         </div>
     )
